aeb3077c7650e4e987ff89593297fcdde273eb48
steadykyu <awakekyu1@gmail.com>
2022-07-20 11:59:40 +0900

    프로그래머스2

--- Algorithm/프로그래머스/src/Solution.java
+++ Algorithm/프로그래머스/src/Solution.java
@@ -1,36 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
-    public int[] solution(int n, int m) {
-        int[] answer = new int[2];
-        int Big = Math.max(n,m);
-        int sml = Math.min(n,m);
+    public String solution(String s) {
 
-        while(true){
-
-            int remain = Big % sml;
-
-            if(remain == 0) {
-              answer[0] = sml;
-              answer[1] = n*m/sml;
-                break;
-            }else{
-                Big = sml;
-                sml = remain;
-            }
-        }
-
-        return answer;
+        return "";
     }
 
     public static void main(String[] args) {
 
         Solution sol = new Solution();
-        for(int i : sol.solution(72,30)){
-            System.out.printf(i + " ");
-        }
 
+        String str = "boo:and:foooo";
+        String[] strarr = str.split("o",-2);
+
+        for(String s : strarr){
+            System.out.println(s);
+        }
+        System.out.println("종료");
     }
 }
-/**
- * https://myjamong.tistory.com/138
- * 유글리드 호제법
- */
