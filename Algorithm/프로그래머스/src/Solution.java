ffc15281a355d6498de6d4cc5d9527e7ede4a477
steadykyu <awakekyu1@gmail.com>
2022-07-21 10:02:35 +0900

    문제풀이_프로

--- Algorithm/프로그래머스/src/Solution.java
+++ Algorithm/프로그래머스/src/Solution.java
@@ -1,16 +1,25 @@
-class Solution {
-    public String solution(String s, int n) {
-        String answer = "";
+import java.util.ArrayList;
 
-        char[] charr = s.toCharArray();
-        for(char ch : charr){
-            for(int i=1; i <= n; i++){
-                if(ch == 'z') ch = 'a';
-                else if(ch == 'Z') ch = 'A';
-                else if(ch == ' ') ch = ' ';
-                else ch++;
+class Solution {
+    public int solution(int n) {
+        int[] arr = new int[n];
+
+        // 0~n-1 까지의 배열
+        for (int i = 0; i < n; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 2; i * i <= n; i++) { // 나누는 값 i
+            if (arr[i] == 0) continue;         // 0으로 초기화 된값은 배수값이므로 스킵
+            for (int j = i + i; j <= n - 1; j += i) { // i를 제외한 i의 배수들은 모두 0으로 초기화
+                arr[j] = 0;
             }
-            answer += String.valueOf(ch);
+        }
+
+        // 소수 세기
+        int answer=0;
+        for(int a : arr){
+            if(a != 0 && a != 1) answer++;
         }
         return answer;
     }
@@ -18,6 +27,6 @@ class Solution {
     public static void main(String[] args) {
 
         Solution sol = new Solution();
-        System.out.println(sol.solution("a B z",4));
+        System.out.println(sol.solution(10));
     }
 }
