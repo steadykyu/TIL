a8a381be945a095ec3ca2c28e04b629ac46a1109
steadykyu <awakekyu1@gmail.com>
2022-07-21 21:47:31 +0900

    프로그래머스2_4

--- Algorithm/프로그래머스/src/Solution.java
+++ Algorithm/프로그래머스/src/Solution.java
@@ -2,17 +2,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 class Solution {
-    public String solution(String s) {
-
-        return s.;
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+        long tolprice =0;
+        for(int i=1; i<=count;i++){
+            tolprice += price * i;
+        }
+        answer = Math.abs(tolprice - money);
+        return answer;
     }
-
-
 
     public static void main(String[] args) {
 
         Solution sol = new Solution();
-        String str = "abcde";
-        System.out.println(sol.solution(str));
+        System.out.println(sol.solution(3,20,4));
+
     }
 }
