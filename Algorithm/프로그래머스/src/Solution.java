53783815da614cee50875b5eaeb3ef399a97aae3
steadykyu <awakekyu1@gmail.com>
2022-07-21 17:44:12 +0900

    프로그래머스2_3

--- Algorithm/프로그래머스/src/Solution.java
+++ Algorithm/프로그래머스/src/Solution.java
@@ -1,32 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
-    public int solution(int n) {
-        int[] arr = new int[n];
+    public String solution(String s) {
 
-        // 0~n-1 까지의 배열
-        for (int i = 0; i < n; i++) {
-            arr[i] = i;
-        }
-
-        for (int i = 2; i * i <= n; i++) { // 나누는 값 i
-            if (arr[i] == 0) continue;         // 0으로 초기화 된값은 배수값이므로 스킵
-            for (int j = i + i; j <= n - 1; j += i) { // i를 제외한 i의 배수들은 모두 0으로 초기화
-                arr[j] = 0;
-            }
-        }
-
-        // 소수 세기
-        int answer=0;
-        for(int a : arr){
-            if(a != 0 && a != 1) answer++;
-        }
-        return answer;
+        return s.;
     }
+
+
 
     public static void main(String[] args) {
 
         Solution sol = new Solution();
-        System.out.println(sol.solution(10));
+        String str = "abcde";
+        System.out.println(sol.solution(str));
     }
 }
