285d6cab4b5c32e5770d8a91233c4a2f768f50c8
steadykyu <awakekyu1@gmail.com>
2022-07-26 16:12:17 +0900

    인프런_자바

--- Algorithm/프로그래머스/src/Solution.java
+++ Algorithm/프로그래머스/src/Solution.java
@@ -1,61 +1,41 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
 
-    public int[] solution(int[] answers) {
-        int len = answers.length;
-        int[] p1 = new int[answers.length];
-        int[] p2 = new int[answers.length];
-        int[] p3 = new int[answers.length];
+        for(int p = 0 ; p < commands.length; p++){
+            // 1. i, j , k 읽어오기 ex) 2, 5, 3
+            int i = commands[p][0];
+            int j = commands[p][1];
+            int k = commands[p][2];
 
-        int k=0; int[] p2arr = {1,3,4,5};
-        int[] p3arr = {3,3,1,1,2,2,4,4,5,5};int k3 = 0;
+            // 2. 배열 자르고 정렬하기 1 4
+            int[] array2 = new int[j-i+1];
+            for(int x = 0; x < array2.length; x++){
+                array2[x] = array[x+i-1];
+            }
 
-        // 값 대입
-        for(int i = 0; i < len; i++){
-            //p1
-            p1[i] = i % 5 + 1;
-            // p2
-            if((i+1) % 2==0) {
-                p2[i] = p2arr[k];
-                k++;
-                k = k % 4; // 0~3
-            }else p2[i] = 2;
-            //p3
-            p3[i] = p3arr[k3];
-            k3++;
-            k3 %= 10; // 0~9
-        }
-//        for(int x : p3){
-//            System.out.printf(x+" ");
-//        }
+            Arrays.sort(array2);
 
-        // 정답개수 세기
-        int[] cntarr = {0,0,0,0};
-        for (int i = 0; i < len; i++) {
-            if (answers[i] == p1[i]) cntarr[1]++;
-            if (answers[i] == p2[i]) cntarr[2]++;
-            if (answers[i] == p3[i]) cntarr[3]++;
+            // 3. k 번쨰 숫자 가져오고 answer에 넣어주기
+            answer[p] = array2[k-1];
         }
 
-        int max = Math.max(Math.max(cntarr[1],cntarr[2]),cntarr[3]);
-        // 결과 출력
-        ArrayList<Integer> al = new ArrayList<Integer>();
-        for(int x=1; x<cntarr.length ; x++){
-            if( cntarr[x] == max) al.add(x);
-        }
-        int[] answer = al.stream().mapToInt(x -> x.intValue()).toArray();
         return answer;
     }
 
     public static void main(String[] args) {
 
         Solution sol = new Solution();
-        int[] arr = {1,2,3,4,5,6,7,8,9,10,11};
-        for(int x : sol.solution(arr)){
-            System.out.println(x);
+        int[] arr = {1, 5, 2, 6, 3, 7, 4};
+        int[][] arr2 = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+
+        for(int t : sol.solution(arr,arr2)){
+            System.out.printf(t +",");
         }
 
     }
