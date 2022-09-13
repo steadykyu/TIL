9dd7390f0d84dbabab029bff07164c5d9642e990
steadykyu <awakekyu1@gmail.com>
2022-09-13 19:13:54 +0900

    번호수정

--- Algorithm/프로그래머스/src/Solution.java
+++ Algorithm/프로그래머스/src/Solution.java
@@ -2,34 +2,23 @@
 import java.util.HashMap;
 
 class Solution {
-        public String solution(String new_id) {
-            String answer = "";
-            String temp = new_id.toLowerCase(); // 1 단계
+    public int solution(int n) {
 
-            temp = temp.replaceAll("[^-_.a-z0-9]","");      // 2단계
-            System.out.println(temp);
-            temp = temp.replaceAll("[.]{2,}",".");  // 3단계
-            temp = temp.replaceAll("^[.]|[.]$",""); // 4단계
-            System.out.println(temp.length());
-            // 5단계
-            if(temp.equals(""))
-                temp+="a";
-            if(temp.length() >=16){
-                temp = temp.substring(0,15);
-                temp=temp.replaceAll("^[.]|[.]$","");
+        int answer =0;
+
+        // 제곱근까지의 약수 i
+        for(int i=1; i<= Math.sqrt(n); i++){
+            if(n % i ==0){
+               answer += i + n/i;
             }
-            if(temp.length()<=2)
-                while(temp.length()<3)
-                    temp+=temp.charAt(temp.length()-1);
-
-            answer=temp;
-            return answer;
         }
+        return answer;
+    }
 
     public static void main(String[] args) {
 
         Solution sol = new Solution();
-        String str = "=.=";
-        System.out.println(sol.solution(str));
+        int num = 12;
+        System.out.println(sol.solution(num));
     }
 }
