a353dc5692b46fac391332bc3e4e5491ed833cf2
steadykyu <awakekyu1@gmail.com>
2022-09-29 19:05:42 +0900

    11번까지 정리함

--- Algorithm/자바알고리즘_인프런/Main.java
+++ Algorithm/자바알고리즘_인프런/Main.java
@@ -1,29 +1,37 @@
 import java.util.*;
-
-class Node{
-    int data;
-    Node lt, rt;
-    public Node(int val){
-        data=val;
-        lt=rt=null;
-    }
-}
 public class Main {
-    Node root;
-
-    int minL = 100;
-    public int DFS(int L, Node root){
-        if(root.lt==null && root.rt==null) return L;
-        else return Math.min(DFS(L+1, root.lt), DFS(L+1, root.rt));
+    static int N, lines, answer=0;
+    static int[][] graph;
+    static int[] ch;    // 접점 통과 유무를 나타냄
+    public void DFS(int v){
+        if(v==N) answer++;
+        else {
+            for(int i=1; i<=N; i++){
+                if(graph[v][i]==1 && ch[i]==0){
+                    // 간선이 존재하고 아직 지나오지 않은 정점일 때만 더 깊이 내려간다.
+                    ch[i]=1;
+                    DFS(i);
+                    ch[i]=0; // 재귀를 마치고 백한경우
+                }
+            }
+        }
     }
 
     public static void main(String[] args){
-        Main tree = new Main();
-        tree.root = new Node(1);
-        tree.root.lt = new Node(2);
-        tree.root.rt = new Node(3);
-        tree.root.lt.lt = new Node(4);
-        tree.root.lt.rt = new Node(5);
-        System.out.println(tree.DFS(0, tree.root));
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        N = kb.nextInt(); // 접점의 수
+        lines = kb.nextInt(); // 간선의 수
+        graph = new int[N +1][lines+1];
+        ch = new int[N +1];
+        for(int i=0; i<lines;i++){
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            graph[a][b] = 1;
+        }
+        // 인접행렬 생성(a->b로 가는 방향 그래프)
+        ch[1] = 1;
+        T.DFS(1);
+        System.out.println(answer);
     }
 }
