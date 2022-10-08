e7ad4db54b2a0d9b35ffad88d58f6be1642f9ebe
steadykyu <awakekyu1@gmail.com>
2022-10-08 19:48:55 +0900

    14번 진행중

--- Algorithm/자바알고리즘_인프런/Main.java
+++ Algorithm/자바알고리즘_인프런/Main.java
@@ -1,53 +1,48 @@
 import java.util.*;
 class Point{
-    int x,y;
+    public int x, y;
     Point(int x, int y){
-        this.x = x;
-        this.y = y;
+        this.x=x;
+        this.y=y;
     }
 }
 class Main {
-    static int n;
-    static int[] dx = {-1, 0, 1, 0, -1,-1,1,1};
-    static int[] dy = {0, 1, 0 ,-1, -1,1,-1,1};
-    static int answer=0;
-    static Queue<Point> Q = new LinkedList<>();
-    public void BFS(int x, int y, int[][] board) {
-        Q.offer(new Point(x,y));
-        while(!Q.isEmpty()){
-            Point tmp = Q.poll();
-            for(int i=0; i<8;i++){
-                int nx = tmp.x+dx[i];
-                int ny = tmp.y+dy[i];
-                if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]==1){
-                    Q.offer(new Point(nx,ny));
-                    board[nx][ny]=0;
-                }
+    static int n, m,len, answer = Integer.MAX_VALUE;
+    static int[] combi;
+    static ArrayList<Point> hs,pz;
+    // 조합구하기
+    public void DFS(int L, int s) {
+        if(L==m){
+            for (int x: combi) {
+                System.out.print(x+" ");
+            }
+            System.out.println();
+        }else {
+            for(int i=s; i < len;i++){
+                combi[L] = i;
+                DFS(L+1, i+1);
             }
         }
+
     }
-    public void solution(int[][] board){
-        for(int i=0; i<n; i++){
-            for (int j = 0; j < n; j++) {
-                if(board[i][j]==1) {
-                    answer++;
-                    board[i][j] =0; // 각 섬의 출발점도 0으로
-                    BFS(i,j,board);
-                }
-            }
-        }
-    }
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         n = kb.nextInt();
-        int[][] board = new int[n][n];
+        m = kb.nextInt();
+        pz = new ArrayList<>(); // 피자 집
+        hs = new ArrayList<>(); // 가정 집
         for(int i=0; i<n; i++){
-            for (int j = 0; j < n; j++) {
-                board[i][j] = kb.nextInt();
+            for (int j=0; j<n; j++) {
+                int tmp = kb.nextInt();
+                if(tmp == 1) hs.add(new Point(i,j));
+                else if(tmp == 2) pz.add(new Point(i,j));
             }
         }
-        T.solution(board);
-        System.out.println(answer);
+        len = pz.size();
+        combi=new int[m];
+        T.DFS(0,0); // pz list에 대하여 조합 경우의 수를 만든다.
+
     }
 }
