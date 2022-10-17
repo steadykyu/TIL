4b99dc66e9de0c77d393eea5ebd862a3b3bd30ae
steadykyu <awakekyu1@gmail.com>
2022-10-17 22:36:57 +0900

    3번

--- Algorithm/자바알고리즘_인프런/Main.java
+++ Algorithm/자바알고리즘_인프런/Main.java
@@ -1,53 +1,28 @@
 import java.util.*;
-
-class Edge implements Comparable<Edge>{
-    public int vex;
-    public int cost;
-
-    public Edge(int vex, int cost) {
-        this.vex = vex;
-        this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Edge o) {
-        return this.cost - o.cost;
-    }
-}
 class Main {
+    static int[] dy; // dynamic 의 약자
+    public int solution(int[] arr){
+        int answer=0;
+        dy=new int[arr.length];
+        dy[0]=1;
+        for(int i=1; i<arr.length; i++){
+            int max=0;
+            for(int j=i-1; j>=0; j--){
+                if(arr[j]<arr[i] && dy[j]>max) max=dy[j];
+            }
+            dy[i]=max+1;
+            answer=Math.max(answer, dy[i]);
+        }
+        return answer;
+    }
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        int n =kb.nextInt();        // 도시의 수(정점의 수)
-        int m = kb.nextInt();       // 도로의 수(간선의 수)
-
-        ArrayList<ArrayList<Edge>> graph = new ArrayList<ArrayList<Edge>>();
-        for(int i=0; i<=n;i++){  // 일부로 idx 맞춤
-            graph.add(new ArrayList<Edge>());
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = kb.nextInt();
         }
-        int[] ch = new int[n+1]; // check 배열
-        for(int i=0; i<m; i++){
-            int a = kb.nextInt();
-            int b = kb.nextInt();
-            int c = kb.nextInt();
-            graph.get(a).add(new Edge(b, c)); // a node -> bnode 갈때 비용 c
-            graph.get(b).add(new Edge(a, c)); // a node -> bnode 갈때 비용 c
-        }
-        int answer = 0;
-        PriorityQueue<Edge> pQ = new PriorityQueue<>();
-        pQ.offer(new Edge(1,0));
-
-        while(!pQ.isEmpty()) {
-            Edge tmp = pQ.poll();
-            int ev = tmp.vex;   // endvertax
-            if(ch[ev]==0){  // 회로 방지
-                ch[ev] =1;
-                answer += tmp.cost;
-                for(Edge ob : graph.get(ev)){
-                    if(ch[ob.vex]==0) pQ.offer(new Edge(ob.vex, ob.cost));
-                }
-            }
-        }
-        System.out.println(answer);
+        System.out.println(T.solution(arr));
     }
 }
